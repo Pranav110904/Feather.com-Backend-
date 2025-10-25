a3b788357b1be25e57989ef6a40eb2f6719cde5a
Pranav <pranavmk11092004@gmail.com>
2025-10-26 00:45:34 +0530

    Add subcategory to main category mapping utility
    Introduces SUBCATEGORY_TO_MAIN mapping and DEFAULT_MAIN_CATEGORY constant for categorizing subcategories into main categories. This utility can be used to standardize category handling across the application.

---
+++ Utils/subcategoryMapping.js
@@ -0,0 +1,21 @@
+export const SUBCATEGORY_TO_MAIN = {
+  "sports": "sports",
+  "fitness_&_health": "sports",
+  "arts_&_culture": "entertainment",
+  "film_tv_&_video": "entertainment",
+  "music": "entertainment",
+  "celebrity_&_pop_culture": "entertainment",
+  "gaming": "entertainment",
+  "fashion_&_style": "entertainment",
+  "travel_&_adventure": "entertainment",
+  "food_&_dining": "entertainment",
+  "diaries_&_daily_life": "entertainment",
+  "family": "entertainment",
+  "relationships": "entertainment",
+  "other_hobbies": "entertainment",
+  "news_&_social_concern": "news",
+  "business_&_entrepreneurs": "news",
+  "science_&_technology": "news",
+  "learning_&_educational": "news"
+};
+export const DEFAULT_MAIN_CATEGORY = "news";
